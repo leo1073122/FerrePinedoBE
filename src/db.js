93fa1c5d759f359f7f2ec1eac47a487e674485de
Leo <leo1073@outlook.es>
2026-02-21 17:39:40 -0300

    corregimos errores de tipeo en controladores y servivcios

--- src/db.js
+++ src/db.js
@@ -4,9 +4,13 @@ import { config } from "dotenv";
 config();
 
 export const pool = createPool({
-  port: process.env.DB_PORT,
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  },
+  connectTimeout: 20000 
 });
